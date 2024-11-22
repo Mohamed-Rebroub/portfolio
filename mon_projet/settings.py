e418e840a4e906c7c70304175c3811c9a033128d
Mohamed REBROUB <srebroub550@gmail.com>
2024-11-22 12:57:10 +0100

    Correction de ALLOWED_HOSTS et configuration Render

--- mon_projet/settings.py
+++ mon_projet/settings.py
@@ -29,9 +29,9 @@ SECRET_KEY = config("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG", default=False, cast=bool)
+DEBUG = False
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="localhost").split(",")
+ALLOWED_HOSTS = ["mohamed-rebroub.onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
