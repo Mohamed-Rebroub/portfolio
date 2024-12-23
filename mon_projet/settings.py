bd4bdd7fd03b4198f9e5f587e2f39fc2344b5f6d
Mohamed-Rebroub <151969075+Mohamed-Rebroub@users.noreply.github.com>
2024-12-24 00:02:53 +0100

    Update settings.py

--- mon_projet/settings.py
+++ mon_projet/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["mohamed-rebroub.onrender.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["mohamed-rebroub-hn4c.onrender.com", "localhost", "127.0.0.1"]
 
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 SECURE_SSL_REDIRECT = True
